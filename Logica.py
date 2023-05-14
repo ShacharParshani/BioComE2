085e4798bdc60920438710de4572ba6c1587b5fe
Liel Sendler <liel819@gmail.com>
2023-05-14 16:16:10 +0300

    add permotation functions and replication

--- Logica.py
+++ Logica.py
@@ -1,3 +1,7 @@
+import copy
+
+import Permutation
+
 class Logica:
     def __init__(self, pCrossover, pMut, k):
         self.pCrossover = pCrossover
@@ -14,6 +18,23 @@ class Logica:
         return 0
     def crossover(self):
         # ...
+        return 0
 
     def mutation(self, p):
         # ...
+        return 0
+
+    def replication(self, p):
+        newInstance = copy.copy(p)
+        print("old: p.fitness = ", p.fitness, "newInstance.fitness = ", newInstance.fitness)
+        p.fitness = 100
+        print("new: p.fitness = ", p.fitness, "newInstance.fitness = ", newInstance.fitness)
+
+        return newInstance
+
+# if __name__ == '__main__':
+#     p = Permutation.Permutation()
+#     p.upgrade_fitness()
+#     l = Logica(95, 5, 20)
+#     l.replication(p)
+
--- Permutation.py
+++ Permutation.py
@@ -1,13 +1,51 @@
+import random
+
 class Permutation:
     def __init__(self):
-        self.permutation = random_order()
-        self.fitness = upgrade_fitness()
+        self.permutation = self.random_order()
+        self.decoded_text = None
+        self.fitness = None
 
     def random_order(self): #random array of the 26 letters
-        /...
+        # Define the list of letters to shuffle
+        letters = [chr(i) for i in range(97, 123)]
+
+        # Shuffle the letters using the random module
+        random.shuffle(letters)
+        return letters
 
     def upgrade_fitness(self): #calculate and upgrade fitness
+        with open('dict.txt', 'r') as f:
+            common_words = f.read()
+
+        self.decoded_text = self.decoding()
+        realWordsCounter = 0
+
+        splitedText = self.decoded_text.split()
+
+        for word in splitedText:
+            if word in common_words:
+                realWordsCounter += 1
+
         # calculation
-        new_fitness =
+        new_fitness = realWordsCounter/len(splitedText)
         self.fitness = new_fitness
-    def decoded_text(self):
+    def decoding(self):
+        with open('enc.txt', 'r') as f:
+            encoded = f.read()
+        decoded_text = ""
+
+        for char in encoded:
+            if char.isalpha():
+                # Subtract the key value from the ASCII code of each character
+                decoded_char = self.permutation[ord(char.lower()) - 97]
+                decoded_text += decoded_char
+            else:
+                decoded_text += char
+
+        return decoded_text
+
+# if __name__ == '__main__':
+#     p = Permutation()
+#     p.upgrade_fitness()
+#     print("permutation ", p.permutation, "\n decoded_text ", p.decoded_text, "\n fitness", p.fitness)
