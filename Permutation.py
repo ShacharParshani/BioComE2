82bb73a782448e34435fa2814874e961e16af953
parshas <shachar.parshani@gamil.com>
2023-05-28 10:11:17 +0300

    add save solution

--- Permutation.py
+++ Permutation.py
@@ -20,36 +20,20 @@ class Permutation:
         return letters
 
     def upgrade_fitness(self): #calculate and upgrade fitness
-        with open('dict.txt', 'r') as f:
-            common_words_file = f.read()
-        common_words = common_words_file.split()
         with open('Letter_Freq.txt', 'r') as f_freq:
             freq = pd.read_csv(f_freq, sep='\t', header=None).iloc[:, 0]
-
         self.decoded_text = self.decoding()
         realWordsCounter = 0
-
         splitedText = self.decoded_text.split()
 
-        for word in splitedText:
-            if word in common_words:
-                # print(word)
-                realWordsCounter += 1
-
         # calculation
-        # print("realWordsCounter: ", realWordsCounter, "len: " , len(splitedText))
-        new_fitness = realWordsCounter/len(splitedText)
-        #new_fitness = (realWordsCounter ** 2) / (len(splitedText) ** 2)
-
-
         y_actual = freq
         y_get = self.cal_freq()
         MSE = np.square(np.subtract(y_actual, y_get)).mean()
         RMSE = math.sqrt(MSE)
         self.RMSE = RMSE
-        new_fitness = realWordsCounter / len(splitedText)
+        new_fitness = self.cal_common_words();
         self.fitness = new_fitness * 1000 + 100 - RMSE * 0.1
-        #self.fitness = new_fitness
     def cal_common_words(self):
         with open('dict.txt', 'r') as f:
             common_words_file = f.read()
@@ -78,6 +62,22 @@ class Permutation:
         # RMSE = math.sqrt(MSE)
         # self.fitness = new_fitness - RMSE
         return new_fitness
+
+    def print_not_in_dict(self):
+        with open('dict.txt', 'r') as f:
+            common_words_file = f.read()
+        common_words = common_words_file.split()
+        #  with open('Letter_Freq.txt', 'r') as f_freq:
+        #      freq = pd.read_csv(f_freq, sep='\t')[0]
+
+        self.decoded_text = self.decoding()
+
+        splitedText = self.decoded_text.split()
+
+        for word in splitedText:
+            if word not in common_words:
+                # print(word)
+                print(word)
     def cal_freq(self):
         freq = [];
         letters = [chr(i) for i in range(97, 123)]
